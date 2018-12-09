c4b9c3d56593a012927bc4a6a0fed295b893185e
= <=>
2018-12-09 18:36:14 +0200

    A user can generate their own card after one select all

--- classes/User.class.php
+++ classes/User.class.php
@@ -1,26 +1,112 @@
 <?php
-	class User
-	{
-		
-		private $_pdo,
-		$_id,
-	 	$_username,
-		$_bio,
-		$_age;
-               
-	
-		public function __construct($fields = array()){
-                    
-                }
-		
-		public function __destruct(){}
-		
-		public function __toString(){}
-		
-		public function update($fields = array())
-		{
-			
-		}
 
-	};
+require_once $_SERVER["DOCUMENT_ROOT"] . '/Matcha/classes/UserCard.class.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/Matcha/config/database.php';
+
+class User {
+
+    private $_pdo,
+            $_id,
+            $_username,
+            $_bio,
+            $_age,
+            $_fname,
+            $_lname,
+            $_email,
+            $_card,
+            $_job;
+
+    public function __construct($fields = array()) {
+        //var_dump($fields);
+        foreach ($fields as $key => $value) {
+            switch ($key) {
+                case "username":
+                    $this->_username = $value;
+                    break;
+                case "id":
+                    $this->_id = $value;
+                    break;
+                case "bio":
+                    $this->_bio = $value;
+                    break;
+                case "birth_date":
+                    $this->_age = $this->calculate_age($value);
+                    break;
+                case "email":
+                    $this->_email = $value;
+                    break;
+                case "first_name":
+                    $this->_fname = $value;
+                    break;
+                case "last_name":
+                    $this->_lname = $value;
+                    break;
+                case "ocupation":
+                    $this->_job = $value;
+                    break;
+            }
+        }
+        $this->_card = new UserCard();
+        $this->_pdo = DB::getConnection();
+    }
+
+    private function convertString($date) {
+        // convert date and time to seconds 
+        $sec = strtotime($date);
+
+
+        $date_new = date("m/d/Y", $sec);
+        return $date_new;
+    }
+
+    private function calculate_age($birthDate) {
+        $birthDate = $this->convertString($birthDate);
+        $birthDate = explode("/", $birthDate);
+        //get age from date or birthdate
+        $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md") ? ((date("Y") - $birthDate[2]) - 1) : (date("Y") - $birthDate[2]));
+        return ($age);
+    }
+
+    public function __destruct() {
+        
+    }
+
+    private function populateCard() {
+        $imgattrs = array(
+            "class" => "d-block w-100 card-picture",
+            "alt" => "./imgs/avatar.png",
+            "type" => "jpg"
+        );
+        $stmt = $this->_pdo->prepare("Select `src`, `type` FROM `images` WHERE user_id=:id");
+        $stmt->bindParam(":id", $this->_id, PDO::PARAM_INT);
+        $stmt->execute();
+        if ($stmt->rowCount() == 0) {
+            $this->_card->add_images("./imgs/avatar.png", true, $imgattrs);
+        } else {
+            while (($res = $stmt->fetch(PDO::FETCH_ASSOC))) {
+                $imgattrs["type"] = $res["type"];
+                $this->_card->add_images($res["src"], false, $imgattrs);
+            }
+        }
+        $this->_card->set_cardText($this->_job);
+        $this->_card->set_Title($this->_fname . " " . $this->_lname . ", " . $this->_age);
+        $this->_card->set_bioText($this->_bio);
+        $this->_card->assemble();
+    }
+
+    public function __toString() {
+
+        $this->populateCard();
+        $ret_string = "";
+        $ret_string .= $this->_card;
+        return $ret_string;
+    }
+
+    public function update($fields = array()) {
+        
+    }
+
+}
+
+;
 ?>
--- index.php
+++ index.php
@@ -5,6 +5,8 @@ if (!isset($_SESSION))
 if (!isset($_SESSION["login"]))
     $_SESSION["login"] = "guest";
 require_once './classes/UserCard.class.php';
+require_once './classes/User.class.php';
+require_once './config/database.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,20 +21,11 @@ require_once './classes/UserCard.class.php';
 
             <div class="card-wrapper">
                 <?php
-                $card = new UserCard();
-
-                $imgattrs = array(
-                    "class" => "d-block w-100 card-picture",
-                    "alt" => "",
-                    "type" => "jpg"
-                );
-                $img = base64_encode(file_get_contents("./imgs/13524498_1355525024475110_341246408285628536_n.jpg"));
-                $card->add_images($img, false, $imgattrs);
-                $card->add_images("./imgs/14138715_10210283076183583_2563936512643780707_o.jpg", true, $imgattrs);
-                $card->set_cardText("WeThinkCode_");
-                $card->set_Title("Fred Dilapisho, 21");
-                $card->assemble();
-                echo $card;
+                $pdo = DB::getConnection();
+                $stmt =$pdo->query("SELECT * FROM `users` WHERE username='Fred_Dee'");
+                $res = $stmt->fetch(PDO::FETCH_ASSOC);
+                $user = new User($res);
+                echo $user;
                 ?>
             </div>
             <?php require_once './includes/footer.inc.php' ?>
