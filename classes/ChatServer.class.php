8578cd3500face883918c714501a73852711bffa
Fred-Dee <fred.dilapisho@gmail.com>
2019-01-07 13:58:30 +0200

    Fix the chat server asap

--- classes/ChatServer.class.php
+++ classes/ChatServer.class.php
@@ -6,16 +6,34 @@ class ChatServer
 {
 	public static $_source = null;
 	
-	public static function getConversation()
+	private static function isMatch($value1, $value2, $string)
+	{
+		//$pattern= "/".$value1.":".$value2."/"
+		if (preg_match("/".$value1.":".$value2."/", $string) || preg_match("/".$value2.":".$value1."/", $string) )
+			return true;
+		return false;
+	}
+	
+	public static function getConversation($username)
 	{
 		if (self::$_source == null)
 			self::init();
-	
+		
+		$keys = array_keys(self::$_source);
+		$myconvos = array();
+		foreach ($keys as $value)
+		{
+			if (self::isMatch($_SESSION["login"], $username, $value))
+				array_push($myconvos, $username);
+		}
+		//$myconvos = array_search($_SESSION["login"], $keys);
+		return $myconvos;
+		
 	}
 	
 	public static function init()
 	{
-		slef::$_source = file_get_contents("./private/chats.txt");
+		self::$_source = json_decode(file_get_contents("./private/chats.txt"), true);
 	}
 	
 }
--- index.php
+++ index.php
@@ -5,12 +5,6 @@ if (!isset($_SESSION))
     session_start();
 if (!isset($_SESSION["login"]))
     $_SESSION["login"] = "guest";
-
-//spl_autolaod_register("./classes/UserCard.class.php");
-//spl_autolaod_register("./classes/User.class.php");
-//require_once './classes/UserCard.class.php';
-//require_once './classes/User.class.php';
-//require_once './config/database.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -31,7 +25,6 @@ if (!isset($_SESSION["login"]))
 			
             <div class="card-wrapper align-middle">
                 <?php
-				//echo $_SERVER["DOCUMENT_ROOT"];
                 include './private/loaddata.php';
                 ?>
             </div>
@@ -48,15 +41,10 @@ if (!isset($_SESSION["login"]))
                 <div class="col chat-section">
 
                     <div class="chat-content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada consectetur bibendum. Cras laoreet sapien diam, sit amet congue metus dapibus sed. Sed pretium volutpat orci ut sagittis. Cras sit amet sapien gravida, vehicula nunc ac, porta ipsum. Morbi sed gravida ante. Nam urna purus, mollis at quam gravida, dictum laoreet mi. Fusce sodales in elit sed faucibus.
-
-                        Fusce et lacus nec nunc ultricies aliquet a nec erat. Vivamus lobortis libero sed massa rhoncus, ac tincidunt purus dignissim. Pellentesque mattis odio a malesuada malesuada. Ut dictum, enim vehicula tristique euismod, odio risus condimentum elit, eu volutpat tortor lectus id risus. Suspendisse ultricies libero velit, eu semper est maximus a. Sed nec lacinia sapien, at tristique arcu. Nullam purus justo, venenatis sit amet mi eget, ultricies varius lacus. Sed id sem quis dui semper varius. Cras quis vehicula turpis. Maecenas non maximus orci, ac viverra mauris. Nam velit libero, faucibus a pharetra sit amet, laoreet at odio. Integer bibendum fermentum arcu vel malesuada. Ut maximus tincidunt odio quis ultricies.
-
-                        Curabitur pulvinar eu ipsum ut volutpat. Nam ac purus id urna posuere consequat at vel mauris. In hac habitasse platea dictumst. Mauris dignissim suscipit magna, eget ullamcorper augue pulvinar et. Mauris leo velit, tincidunt in erat ac, aliquam tempus leo. Mauris lacinia orci eu ipsum egestas scelerisque. Suspendisse finibus, ex id condimentum egestas, felis lorem blandit sem, at interdum nibh lorem nec velit. Etiam mollis, lectus accumsan accumsan consequat, magna odio faucibus risus, ac congue odio erat sit amet mauris. Vestibulum sed dui nec mi laoreet facilisis. Nunc rutrum dapibus dictum. Vivamus ut condimentum felis, at dictum risus. Sed et euismod tortor, nec rutrum dolor.
-
-                        Aliquam bibendum ut arcu in porta. Nulla vestibulum velit quis lacus posuere, venenatis ullamcorper neque elementum. Donec facilisis, diam et faucibus volutpat, leo augue congue ligula, sed cursus lectus tortor sit amet eros. Sed pretium egestas dui, at interdum lacus. Suspendisse viverra fermentum vulputate. Maecenas ut elementum eros, nec mollis arcu. Praesent tincidunt, magna non hendrerit dapibus, ipsum libero pellentesque neque, a tempus felis nisi eget libero. Aliquam molestie urna sed commodo ornare. Mauris condimentum nisi vel tellus hendrerit congue luctus et est. Integer egestas est in nunc consectetur malesuada. Integer vulputate pretium pretium. Proin sodales leo vitae neque rutrum, a luctus velit varius. Pellentesque a risus et libero suscipit egestas nec rhoncus turpis.
-
-                        Vestibulum dapibus nec leo ac feugiat. Nullam malesuada tristique lacus, in blandit nulla tempus a. Sed sed vehicula metus. Nulla mattis neque sit amet justo pellentesque, in rhoncus eros suscipit. Vivamus hendrerit condimentum tortor, id eleifend velit fermentum et. Sed ante risus, sagittis vel elit ac, dictum porta lacus. Sed efficitur tortor mi, tincidunt pharetra ipsum accumsan ut. Sed fringilla venenatis efficitur. Integer blandit rutrum arcu vitae malesuada. Donec pharetra enim ac tortor pellentesque dictum. Nunc eu posuere arcu. Nulla et hendrerit purus. Ut consectetur lacus et nibh laoreet, eget finibus ex sollicitudin. Maecenas in turpis tellus. Donec scelerisque, neque vitae scelerisque interdum, lectus dui faucibus nisi, eu tincidunt purus nunc et ex. 
+   						<?php
+							$source = ChatServer::getConversation("Tester");
+							var_dump ($source);
+						?>
                     </div>
                     <div class="card-wrapper align-middle profile-browse">
                     </div>
